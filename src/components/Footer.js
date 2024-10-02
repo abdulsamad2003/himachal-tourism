e0da0bad6c54e81170431cc044272b3572402098
user <samadsufiyan777888666@gmail.com>
2024-10-02 20:12:23 +0530

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,11 @@ const Footer = () => {
         <div className="content">
           <h1>Himachal Tourism</h1>
           <p>
-              address: near stop n shop, Kelti, Himachal Pradesh 171003
+            Our primary goal is to bridge the gap between travelers and local
+            property owners and service providers. We are committed to
+            establishing strong connections that ensure all potential travelers
+            to Himachal Pradesh can enjoy direct, seamless interactions with the
+            people who matter most to their journey{" "}
           </p>
         </div>
         <div className="social-icon">
@@ -30,63 +34,83 @@ const Footer = () => {
             </span>
           </a>
           <a target="_blank" href="https://www.instagram.com/mybharattrips/">
-          <span className="icon">
-            <FaInstagram color="white" size={"15px"} />
-          </span>
+            <span className="icon">
+              <FaInstagram color="white" size={"15px"} />
+            </span>
           </a>
-          <a target="_blank" href="https://www.linkedin.com/in/my-bharat-trips-023a73222/?originalSubdomain=in">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/my-bharat-trips-023a73222/?originalSubdomain=in"
+          >
             <span className="icon">
               <FaLinkedinIn color="white" size={"15Px"} />
             </span>
           </a>
-          
-          <a target="_blank"  href={`https://wa.me/+917836098136`}>
+
+          <a target="_blank" href={`https://wa.me/+917836098136`}>
             <span className="icon">
               <FaWhatsapp color="white" size={"15px"} />
             </span>
           </a>
-        
         </div>
       </main>
 
       <main className="footer-content middle">
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="#packages">Packages</Link>
-          </li>
-          <li>
-            <Link href="#destinations">Destination</Link>
-          </li>
-          <li>
-            <Link href="#deals">Deals</Link>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <Link href="#about">About Us</Link>
-          </li>
-          <li>
-            <Link href="#services">Privacy Policy</Link>
-          </li>
-          <li>
-            <Link href="#expertise">Contact Us</Link>
-          </li>
-          <li>
-            <Link href="/contact">Discounts</Link>
-          </li>
-        </ul>
+        <div className="content">
+          <h1>Links</h1>
+          <div className="links">
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="#packages">Packages</Link>
+              </li>
+              <li>
+                <Link href="#destinations">Destination</Link>
+              </li>
+              <li>
+                <Link href="#deals">Deals</Link>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <Link href="#about">About Us</Link>
+              </li>
+              <li>
+                <Link href="#services">Privacy Policy</Link>
+              </li>
+              <li>
+                <Link href="#expertise">Contact Us</Link>
+              </li>
+              <li>
+                <Link href="/contact">Discounts</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </main>
       <main className="footer-content right">
         <div className="content">
-          <h1>Subscribe Newsletter</h1>
-          <p>Subscribe to get exciting deals and offers</p>
-        </div>
-        <div className="container">
-          <input type="text" placeholder="Enter Email" />
-          <input type="submit" value="Subscribe" />
+          <h1>Details</h1>
+          <div className="links">
+            <ul>
+              <li>
+                <span>Email</span>
+                <Link href="/">sales@himachaltourismguide.in</Link>
+              </li>
+              <li>
+                <span>Phone no:</span>
+                <Link href="tel:+91 7836098136">+91 7836098136</Link>
+              </li>
+              <li className="hover">
+                <span>Address</span>
+                <Link href="tel:+91 7836098136">
+                  Near stop n shop, Kelti, Himachal Pradesh Pin-code:171003
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </main>
     </footer>
