a85dfb29a761f8ee9cee65b425a69fe49b83eeb0
user <samadsufiyan777888666@gmail.com>
2024-10-05 13:34:58 +0530

    add gtag.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,11 @@ import GetInTouch from "../components/GetInTouch";
 import PopularDestination from "../components/PopularDestination";
 import DealAndOffers from "../components/DealAndOffers";
 import WhyChooseUs from "../components/WhyChooseUs";
-import ReactGA from "react-ga";
+// import ReactGA from "react-ga";
 
 
-const TRACKING_ID = "AW-10780195999";
-ReactGA.initialize(TRACKING_ID)
+// const TRACKING_ID = "AW-10780195999";
+// ReactGA.initialize(TRACKING_ID)
 export default function Home() {
   return (
    <>
